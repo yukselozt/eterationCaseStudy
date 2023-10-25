74685f270be185fd8aa069df2a1ebf6f5129e50f
yukselozt <ykslztrk9@gmail.com>
2023-10-25 14:49:21 +0300

    (+)Add bottomNavigationTabbar Icons

--- src/navigation/BottomNavigator.js
+++ src/navigation/BottomNavigator.js
@@ -4,15 +4,50 @@ import { BasketScreen } from "./screens/BasketScreen";
 import { FavouritesScreen } from "./screens/FavouritesScreen";
 import { SettingsScreen } from "./screens/SettingsScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome"; // İkon setini seçin
+
 export const BottomNavigator = () => {
   const Tabs = createBottomTabNavigator();
 
   return (
     <Tabs.Navigator initialRouteName="Home">
-      <Tabs.Screen name="Home" component={HomeScreen} />
-      <Tabs.Screen name="Basket" component={BasketScreen} />
-      <Tabs.Screen name="Favourites" component={FavouritesScreen} />
-      <Tabs.Screen name="Settings" component={SettingsScreen} />
+      <Tabs.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" size={35} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Basket"
+        component={BasketScreen}
+        options={{
+          tabBarBadge: 5,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="shopping-cart" size={35} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Favourites"
+        component={FavouritesScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="star" size={35} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" size={35} color={color} />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 };
